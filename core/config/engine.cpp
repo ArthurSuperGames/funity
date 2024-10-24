03b6d08cce5b3480b5b249bfdd9f3acb62c3156e
ArthurSuperGames <artthurgamerfeijo@gmail.com>
2024-10-24 20:14:56 -0300

    engine.cpp
    engine

--- core/config/engine.cpp
+++ core/config/engine.cpp
@@ -3,12 +3,12 @@
 /**************************************************************************/
 /*                         This file is part of:                          */
 /*                             FUNITY ENGINE                               */
-/*                        https://godotengine.org                         */
+/*                        https://funityengine.org                         */
 /**************************************************************************/
 /* Copyright (c) 2014-present FUNITY Engine contributors (see AUTHORS.md). */
 /* Copyright (c) 2007-2014 Juan Linietsky, Ariel Manzur.
-*/YORK SITE GODOT ENGINE
-/*                                                                        */
+/* York Engine Godot Engine 
+*/
 /* Permission is hereby granted, free of charge, to any person obtaining  */
 /* a copy of this software and associated documentation files (the        */
 /* "Software"), to deal in the Software without restriction, including    */
